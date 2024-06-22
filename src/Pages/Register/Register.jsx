64498c70a6cf37c100d9b29b084f0fbdee835cd2
farhan-mazumder <farhanmazumder75@gmail.com>
2024-06-23 03:50:05 +0600

    Registation page design part has been added.

---
+++ src/Pages/Register/Register.jsx
@@ -0,0 +1,283 @@
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { FaEye, FaEyeSlash, FaGithub } from "react-icons/fa";
+import Swal from "sweetalert2";
+import { Link, useNavigate } from "react-router-dom";
+import { ImSpinner9 } from "react-icons/im";
+import { FcGoogle } from "react-icons/fc";
+// import Loding from "../Loding/Loding";
+// import useAuth from "../../Hooks/useAuth";
+// import { useMutation } from "@tanstack/react-query";
+// import useAxiosPub from "../../Hooks/useAxiosPub";
+// import useAxiosSec from '../../Hooks/useAxiosSec';
+
+export default function Register() {
+  const isLoading = false;
+  //   const axioss = useAxiosPub();
+  // const axiosss = useAxiosSec();
+  const [errPass, setErrPass] = useState(false);
+  const [imgErr, setImgErr] = useState(null);
+  const [eye, setEye] = useState(false);
+  const naviget = useNavigate();
+
+  //   const { mutateAsync } = useMutation({
+  //     mutationFn: async ({ users }) => {
+  //       const { data } = await axioss.post("/new-user", users);
+  //       console.log(data);
+  //     },
+  //   });
+  // const { mutateAsync: mutateAsyncJwt } = useMutation({
+  //   mutationFn: async ({ userEmail }) => {
+  //     const { data } = await axiosss.post('/jwt', userEmail);
+  //     console.log(data);
+  //   },
+  // });
+
+  //   const {
+  //     emlPassRegister,
+  //     gitHubLogin,
+  //     googleLogin,
+  //     userDta,
+  //     profileUpdate,
+  //     setLoading,
+  //     isLoading,
+  //   } = useAuth();
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = async (data) => {
+    setImgErr(false);
+    setErrPass(false);
+    if (data.Password !== data.Confirm_Password) {
+      setErrPass(true);
+      return;
+    }
+    const photo = data.photo[0];
+    if (photo.name === "" || photo.size === 0) {
+      setImgErr(true);
+      return;
+    }
+    const name = data.Name;
+    const email = data.Email;
+    const password = data.Password;
+
+    const fromImg = new FormData();
+    fromImg.append("image", photo);
+
+    // reset();
+  };
+  // console.log(errors);
+
+  // all Social Login
+  //   const socialLogin = (socialLogin) => {
+  //     socialLogin()
+  //       .then(async (result) => {
+  //         const user = result.user;
+
+  //         const userName = user.displayName;
+  //         const userEmail = user.email;
+  //         const userPhoto = user.photoURL;
+  //         const role = "user";
+  //         const badge = "Bronze";
+  //         const users = {
+  //           userName,
+  //           userEmail,
+  //           userPhoto,
+  //           role,
+  //           badge,
+  //         };
+  //         await mutateAsync({ users });
+
+  //         naviget(location?.state ? location.state : "/");
+  //         console.log(user);
+  //         Swal.fire({
+  //           title: "Good job!",
+  //           text: "Your account has been successfully logged in.",
+  //           icon: "success",
+  //         });
+  //       })
+  //       .catch((error) => {
+  //         const errorMessage = error.message;
+  //         setLoading(false);
+  //         console.log(errorMessage);
+  //         Swal.fire({
+  //           title: "Oops...!",
+  //           text: `Sorry, your account could not be Login ! "${errorMessage.message}"`,
+  //           icon: "error",
+  //         });
+  //       });
+  //   };
+
+  return (
+    <div className="min-h-screen text-white bg-slate-800">
+      <div className="w-10/12 py-10 mx-auto">
+        <h1 className="pt-10 pb-8 font-mono text-3xl font-bold text-center underline sm:text-4xl">
+          SignUp
+        </h1>
+        <div className="w-full md:w-[500px] mx-auto">
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="flex flex-col gap-5"
+          >
+            <div>
+              <input
+                className="w-full px-3 py-2 bg-transparent border rounded shadow-md shadow-slate-500"
+                type="text"
+                placeholder="Name"
+                {...register("Name", { required: true, maxLength: 20 })}
+              />
+
+              {errors.Name && (
+                <span className="text-red-600">Please Input Your Name</span>
+              )}
+            </div>
+            <div>
+              <div className="relative">
+                <label
+                  htmlFor="img"
+                  className={`absolute left-0 top-0 bg-slate-600 px-3.5 py-[11px] text-base botder text-white border border-r-0 rounded-l cursor-pointer`}
+                >
+                  Choose Profile
+                </label>
+                <input
+                  id="img"
+                  className="w-full px-3 py-2 bg-transparent border rounded shadow-md shadow-slate-500 pl-9"
+                  type="file"
+                  accept="image/*"
+                  placeholder="Name"
+                  {...register("photo", { required: true })}
+                />
+              </div>
+              {errors.photo && (
+                <span className="text-red-600">Please Upload Your Photo</span>
+              )}
+              {imgErr && (
+                <span className="text-red-600">Please Upload Your Photo</span>
+              )}
+            </div>
+            <div>
+              <input
+                className="w-full px-3 py-2 bg-transparent border rounded shadow-md shadow-slate-500"
+                type="email"
+                placeholder="Email"
+                {...register("Email", {
+                  required: true,
+                  pattern: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/i,
+                })}
+              />
+              {errors.Email?.type === "required" && (
+                <p className="text-red-600">Please Input Your Email.</p>
+              )}
+              {errors.Email?.type === "pattern" && (
+                <p className="text-red-600">Invalid Email</p>
+              )}
+            </div>
+            <div>
+              <div className="relative">
+                <span className="absolute text-xl -translate-y-1/2 cursor-pointer right-3 top-1/2">
+                  {eye ? (
+                    <span onClick={() => setEye(!eye)}>
+                      <FaEyeSlash />
+                    </span>
+                  ) : (
+                    <span onClick={() => setEye(!eye)}>
+                      <FaEye />
+                    </span>
+                  )}
+                </span>
+                <input
+                  className="w-full px-3 py-2 bg-transparent border rounded shadow-md shadow-slate-500"
+                  type={eye ? "text" : "password"}
+                  placeholder="Password"
+                  {...register("Password", {
+                    required: true,
+                    max: 20,
+                    min: 6,
+                    pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).+$/,
+                  })}
+                />
+              </div>
+              {errors.Password?.type === "required" && (
+                <p className="text-red-600">Please Input a Password.</p>
+              )}
+              {errors.Password?.type === "min" && (
+                <p className="text-red-600">Password must be 6 word</p>
+              )}
+              {errors.Password?.type === "max" && (
+                <p className="text-red-600">Password must be less 20 word</p>
+              )}
+              {errors.Password?.type === "pattern" && (
+                <p className="text-red-600">
+                  Password must be uppercase, lowercase & number
+                </p>
+              )}
+            </div>
+            <div>
+              <input
+                className="w-full px-3 py-2 bg-transparent border rounded shadow-md shadow-slate-500"
+                type="password"
+                placeholder="Confirm Password"
+                {...register("Confirm_Password", { required: true })}
+              />
+              {errors.Confirm_Password?.type === "required" && (
+                <p className="text-red-600">Confirm Password.</p>
+              )}
+              {errPass && (
+                <p className="text-red-600">Password is not matched!</p>
+              )}
+            </div>
+            <button
+              //   disabled={isLoading}
+              className="w-full py-2 font-semibold duration-200 border rounded shadow-md cursor-pointer shadow-slate-400 hover:shadow-lg hover:shadow-slate-200"
+            >
+              {isLoading ? (
+                <ImSpinner9 className="mx-auto text-2xl animate-spin" />
+              ) : (
+                "Sign up"
+              )}
+            </button>
+          </form>
+          <p className="pt-3 text-slate-800 dark:text-slate-100">
+            Already have an account?{" "}
+            <Link to={"/login"} className="underline text-mClr">
+              Login
+            </Link>
+          </p>
+          <div>
+            <div className="relative flex items-center justify-center w-full h-5 my-5">
+              <div className="h-[1px] w-full bg-white"></div>
+              <span className="absolute px-3 font-medium -translate-x-1/2 text-slate-100 bg-slate-800 left-1/2">
+                or
+              </span>
+            </div>
+            <div className="flex flex-col w-full gap-2 lg:flex-row xl:gap-3 text-slate-900 dark:text-stone-100">
+              <button
+                disabled={isLoading}
+                // onClick={() => socialLogin(googleLogin)}
+                className="flex items-center justify-center w-full gap-1 px-1 py-2 font-medium border rounded-md hover:shadow-lg shadow-indigo-900/20 border-mClr"
+              >
+                <span className="text-2xl ">
+                  <FcGoogle />
+                </span>
+                Login With Google
+              </button>
+              <button
+                disabled={isLoading}
+                // onClick={() => socialLogin(gitHubLogin)}
+                className="flex items-center justify-center w-full gap-1 px-1 py-2 font-medium border rounded-md hover:shadow-lg shadow-blue-500/20 border-mClr"
+              >
+                <span className="text-2xl text-mClr dark:text-white">
+                  <FaGithub />
+                </span>
+                Login With GitHub
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../Layouts/MainLayout/MainLayout";
 import HomePage from "../Pages/Home/HomePage/HomePage";
+import Register from "../Pages/Register/Register";
 
 const router = createBrowserRouter([
   {
@@ -12,6 +13,10 @@ const router = createBrowserRouter([
         element: <HomePage />,
       },
     ],
+  },
+  {
+    path: "/register",
+    element: <Register />,
   },
 ]);
 
