65ccfe4bd1038d75f3d8a0bba64101f6bf48346d
farhan-mazumder <farhanmazumder75@gmail.com>
2024-06-23 02:39:46 +0600

    Choose Your Plan section design part has been added.

--- src/Pages/Home/HomePage/HomePage.jsx
+++ src/Pages/Home/HomePage/HomePage.jsx
@@ -1,11 +1,13 @@
 import Carousel from "../Carousel/Carousel";
 import ContactUs from "../ContactUs/ContactUs";
+import PricingSection from "../PricingSection/PricingSection";
 
 const HomePage = () => {
   return (
     <div>
       <Carousel />
       <div className="w-11/12 xl:w-10/12 mx-auto max-w-[1700px]">
+        <PricingSection />
         <ContactUs />
       </div>
     </div>
