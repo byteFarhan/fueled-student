320328aa3a0a71cf04aa87f073cd28cdf0665202
farhan-mazumder <farhanmazumder75@gmail.com>
2024-06-23 00:37:17 +0600

    Initial project setup and resources has been added.

---
+++ src/Routes/Router.jsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../Layouts/MainLayout/MainLayout";
+import HomePage from "../Pages/Home/HomePage/HomePage";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+    ],
+  },
+]);
+
+export default router;
