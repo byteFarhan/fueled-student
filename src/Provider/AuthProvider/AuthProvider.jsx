561c60f5fe2b2d8ea71cf1349218fa70aab9e8ee
farhan-mazumder <farhanmazumder75@gmail.com>
2024-06-23 09:12:00 +0600

    Add all authentication related api and functionallity in AuthProvider.

--- src/Provider/AuthProvider/AuthProvider.jsx
+++ src/Provider/AuthProvider/AuthProvider.jsx
@@ -1,5 +1,86 @@
+/* eslint-disable react/prop-types */
+import {
+  GithubAuthProvider,
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  updateProfile,
+} from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
+import auth from "../../firebase/firebase.config";
+import useAxiosPublic from "../hooks/useAxiosPublic";
 export const AuthContext = createContext(null);
 const AuthProvider = ({ children }) => {
+  const axiosPublic = useAxiosPublic();
+
+  // const axiosSecure = useAxiosSecure();
+  const githubProvider = new GithubAuthProvider();
+  const googleProvider = new GoogleAuthProvider();
+
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  const createUser = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+  const signInUser = (email, password) => {
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+  const updateUserProfile = (user, update) => {
+    return updateProfile(user, update);
+  };
+  const googleLogin = () => {
+    return signInWithPopup(auth, googleProvider);
+  };
+
+  const githubLogin = () => {
+    return signInWithPopup(auth, githubProvider);
+  };
+  const logOut = () => {
+    return signOut(auth);
+  };
+
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
+      if (currentUser) {
+        setUser(currentUser);
+        setLoading(false);
+
+        // if user exist then issue a token ===============================
+        const userEmail = currentUser?.email || user?.email;
+        const loggedUser = { email: userEmail };
+        axiosPublic.post("/jwt", loggedUser).then((res) => {
+          if (res.data.token) {
+            localStorage.setItem("access-token", res.data.token);
+          }
+        });
+      } else {
+        localStorage.removeItem("access-token");
+        setUser(null);
+        setLoading(false);
+      }
+    });
+
+    return () => {
+      unSubscribe();
+    };
+  }, [axiosPublic, user?.email]);
+
+  const authInfo = {
+    user,
+    createUser,
+    signInUser,
+    updateUserProfile,
+    googleLogin,
+    githubLogin,
+    logOut,
+    loading,
+    setUser,
+    setLoading,
+  };
   return (
     <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
   );
